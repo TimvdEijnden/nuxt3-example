c310f2b5c31b6f8d7551519bb3f329f230a0b930
Tim van den Eijnden <tim@passionatepeople.io>
2024-03-07 12:55:01 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Welcome to nuxt 3</h1>
+    <p>Hi {{ name }}, this is a simple example of a Nuxt 3 app</p>
+  </div>
+</template>
+
+<script setup>
+// get name from the query string
+const route = useRoute()
+const name = route.query.name || 'there'
+</script>
+
+<style>
+body {
+  font-family: 'Arial';
+}
+h1 {
+  color: green;
+}
+</style>
